90533b6176e8ffef0f43ffe83e3283d151ffbbd4
VLuth <vierradante@gmail.com>
2025-01-31 15:07:31 +0700

    new

---
+++ BAB2/Explore ulang string/test.py
@@ -0,0 +1,16 @@
+# contoh 1
+nama_produk = 'Sepatu Niko'
+kategori_produk = "Busana Pria"
+print(nama_produk)
+print(kategori_produk)
+
+# contoh 2
+deskripsi_produk = '''Sepatu Niko merupakan sepatu kasual yang aerodinamis.
+Sepatu ini tersedia dalam berbagai macam ukuran. Untuk informasi lebih lanjut
+hubungi kami di no. 083178-xxxxxxxx'''
+print(deskripsi_produk)
+
+#contoh 3
+komentar_terbaik_produk = """Sepatu Niko nyama dipakai dan
+bahannya tidak mudah kotor"""
+print(komentar_terbaik_produk)
---
+++ BAB2/Useful Tips & Tricks/part1.py
@@ -0,0 +1,20 @@
+# Tuple
+print(">>> Tuple")
+tuple_menu = ('Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak')
+jumlah_menu = tuple_menu
+print(jumlah_menu)
+
+# list
+print(">>> List")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+jumlah_menu = list_menu
+print(jumlah_menu)
+
+# Konfersi Tipe Data
+print(">>> Konfersi tipe data")
+list_buah = ['Apel', 'Apel', 'Jeruk', 'Markisa', 'Jeruk', 'Markisa', 'Apel']
+set_buah = set(list_buah)
+print(set_buah)
+list_buah = list(set_buah)
+list_buah.sort()
+print(list_buah)
